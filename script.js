6ff927f6fe6c134c7f9a7fb750614efdf24ba657
Marco-Rocco <marcorocco28@icloud.com>
2025-02-25 16:21:56 +0100

    feat: print 'fizz' for every number multiple of 3

--- script.js
+++ script.js
@@ -2,4 +2,9 @@ console.log('fizzbuzz');
 
 for (let counter = 1; counter <= 100; counter++) {
     console.log(counter)
+
+    if (counter % 3 === 0) {
+        let counter = 'fizz'
+        console.log(counter);
+    }
 }
