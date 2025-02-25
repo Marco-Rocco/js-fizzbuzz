283a432755cf626a3b2431c4ae72e08337840db9
Marco-Rocco <marcorocco28@icloud.com>
2025-02-25 16:31:25 +0100

    feat: printn 'fizzbuzz' for every number muliple of 3 and 5

--- script.js
+++ script.js
@@ -3,12 +3,17 @@ console.log('fizzbuzz');
 for (let counter = 1; counter <= 100; counter++) {
     console.log(counter)
 
-    if (counter % 3 === 0) {
+    if (counter%5 === 0 && counter%3 === 0) {
+        let counter = 'fizzbuzz'
+        console.log(counter);
+    }
+
+    else if (counter%3 === 0) {
         let counter = 'fizz'
         console.log(counter);
     }
 
-    else if (counter % 5 === 0) {
+    else if (counter%5 === 0) {
         let counter = 'buzz'
         console.log(counter);
     }
